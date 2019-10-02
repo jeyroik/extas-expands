1895fd6478790cbf28aa793d70d1462ec49fbf13
jeyroik <jeyroik@gmail.com>
2019-10-02 18:43:08 +0300

    use protocol phd

--- src/components/protocols/ProtocolExpand.php
+++ src/components/protocols/ProtocolExpand.php
@@ -2,7 +2,6 @@
 namespace extas\components\protocols;
 
 use extas\interfaces\expands\IExpandingBox;
-use Psr\Http\Message\RequestInterface;
 
 /**
  * Class ProtocolExpand
@@ -14,64 +13,7 @@ use Psr\Http\Message\RequestInterface;
  * @package extas\components\protocols
  * @author jeyroik@gmail.com
  */
-class ProtocolExpand extends Protocol
+class ProtocolExpand extends ProtocolParameterHeaderDefault
 {
-    const HEADER__PREFIX = 'x-extas-';
-
-    /**
-     * @param array $args
-     * @param RequestInterface $request
-     */
-    public function __invoke(array &$args = [], RequestInterface $request)
-    {
-        $fromHeader = $this->grabHeaders($request);
-        $fromParameter = $this->grabParameters($request);
-        $default = $this->getDefault();
-
-        if (!isset($args[IExpandingBox::FIELD__EXPAND])) {
-            $args[IExpandingBox::FIELD__EXPAND] = is_null($fromParameter)
-                ? (is_null($fromHeader)
-                    ? $default
-                    : $fromHeader)
-                : $fromParameter;
-        }
-    }
-
-    /**
-     * @return string
-     */
-    protected function getDefault(): string
-    {
-        return getenv('EXTAS__PROTOCOL_EXPAND__DEFAULT') ?: '';
-    }
-
-    /**
-     * @param RequestInterface $request
-     *
-     * @return null|string
-     */
-    protected function grabHeaders(RequestInterface $request): ?string
-    {
-        $headerPrefix = getenv('EXTAS__PROTOCOL_EXPAND__HEADER_PREFIX') ?: static::HEADER__PREFIX;
-
-        $headerName = $headerPrefix . IExpandingBox::FIELD__EXPAND;
-        $headers = $request->getHeader($headerName);
-        if (count($headers)) {
-            return array_shift($headers);
-        }
-
-        return null;
-    }
-
-    /**
-     * @param RequestInterface $request
-     *
-     * @return null|string
-     */
-    protected function grabParameters(RequestInterface $request): ?string
-    {
-        parse_str($request->getUri()->getQuery(), $queryParams);
-
-        return $queryParams[IExpandingBox::FIELD__EXPAND] ?? null;
-    }
+    protected $protocolKey = IExpandingBox::FIELD__EXPAND;
 }
